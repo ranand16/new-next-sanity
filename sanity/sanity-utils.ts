75bfe2f00e03f60e5a1546d19d38778aba945e36
Rishabh Anand <ranand16@gmail.com>
2023-04-08 15:20:09 +0530

    last commit

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -1,6 +1,7 @@
 import { Project } from "@/types/Project";
 import { createClient, groq } from "next-sanity";
 import { createClientConfig } from "./config/client-config";
+import { Page } from "@/types/Page";
 
 export async function getProjects(): Promise<Project[]> {
   const client = createClient(createClientConfig);
@@ -29,6 +30,32 @@ export async function getProject(slug: string): Promise<Project> {
         gitUrl, 
         "slug":slug.current, 
         "image": image.asset->url,
+        content
+    }`,
+    { slug }
+  );
+}
+
+export async function getPages(): Promise<Page[]> {
+  const client = createClient(createClientConfig);
+  return client.fetch(
+    groq`*[_type=="page"]{
+        _id, 
+        _createdAt, 
+        title, 
+        "slug":slug.current, 
+    }`
+  );
+}
+
+export async function getPage(slug: string): Promise<Page> {
+  const client = createClient(createClientConfig);
+  return client.fetch(
+    groq`*[_type=="page" && slug.current == $slug][0]{
+        _id, 
+        _createdAt, 
+        title, 
+        "slug":slug.current, 
         content
     }`,
     { slug }
